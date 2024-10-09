4dd09a743da1e5d0791b604cee39d3c75ea45afd
vietnq1-nals <vietnq1@nal.vn>
2024-10-09 14:34:16 +0700

    update version rails 7.1 to 7.2.1

--- lib/apartment.rb
+++ lib/apartment.rb
@@ -12,7 +12,7 @@ require_relative 'apartment/active_record/connection_handling'
 require_relative 'apartment/active_record/schema_migration'
 require_relative 'apartment/active_record/internal_metadata'
 
-if ActiveRecord.version.release >= Gem::Version.new('7.1')
+if ActiveRecord.version.release >= Gem::Version.new('7.2.1')
   require_relative 'apartment/active_record/postgres/schema_dumper'
 end
 
